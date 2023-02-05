c2472e3c3c20e2d55f0241daf631fdc26ec3a57f
Darshan <trivedidarshan30@gmail.com>
2023-02-05 15:30:26 +0530

    Added airport seeders and db sync

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
+
+const db = require('./models/index')
 
 const setupAndStartServer = async () => {
     // create the express object
@@ -14,8 +15,12 @@ const setupAndStartServer = async () => {
 
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({ alert: true })
+        }
+
     })
 }
 
